277c95602797829f069851948a7cb55c6c53c30a
Mauro-hdz <47486440+CaptainHdz@users.noreply.github.com>
2021-07-28 00:33:27 -0500

    Added new email view, search view, added email action

--- Controllers/EmailsController.cs
+++ Controllers/EmailsController.cs
@@ -25,6 +25,12 @@ namespace MvcWebApp.Controllers
             return View(await _context.Email.ToListAsync());
         }
 
+        //GET: Emails/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: Emails/Details/5
         public async Task<IActionResult> Details(int? id)
         {
--- Data/Migrations/20210727020604_InitialDBSetup.Designer.cs
+++ Data/Migrations/20210727020604_InitialDBSetup.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcWebApp.Data;
 
 namespace MvcWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210727020604_InitialDBSetup")]
+    partial class InitialDBSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
